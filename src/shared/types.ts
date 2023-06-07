48afa24e5f67b507cb8437d4789712d722d21da4
Phan M. Hung <pmhungpro1@gmail.com>
2023-06-07 12:41:03 +0700

    feat: add types

---
+++ src/shared/types.ts
@@ -0,0 +1,16 @@
+
+// Define the Product type
+type Product = {
+    id: string;
+    title: string;
+    price: number;
+    
+  };
+  
+  // Define the Category type
+type Category = {
+    id: string;
+    name: string;
+    
+};
+  
