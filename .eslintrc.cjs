af3f5cec936a1270644f2f442de53b496f2e8933
Phan M. Hung <pmhungpro1@gmail.com>
2023-06-07 12:40:55 +0700

    build: setup config eslint and vite to build

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,34 @@ module.exports = {
   ],
   plugins: ['@typescript-eslint'],
   rules: {
-    // Ваши правила ESLint
+    // Правила Feature-Sliced Design
+    'import/no-cycle': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+        'newlines-between': 'always',
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+    'import/no-named-export': 'off',
+    'import/no-unused-modules': [
+      'error',
+      {
+        unusedExports: true,
+        missingExports: true,
+        ignoreExports: ['**/*.types.ts'],
+      },
+    ],
+
+    // Правила TypeScript
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    // Правила React
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
   settings: {
     react: {
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Rename the output folder from "dist" to "build"
+    assetsDir: 'static', // Rename the assets folder from "assets" to "static"
+    rollupOptions: {
+      output: {
+        assetFileNames: '[ext]/[name].[hash][extname]', // Output assets with subfolders based on file extension
+      },
+    },
+  },
 })
