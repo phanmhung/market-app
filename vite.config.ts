6f7a364ff17b8773cb9c8bff9255cfe1ee172670
Phan M. Hung <pmhungpro1@gmail.com>
2023-06-15 12:16:58 +0700

    feat: PWA update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Your App Name',
+        short_name: 'App',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+      },
+    }),
+  ],
   build: {
     outDir: 'build', // Rename the output folder from "dist" to "build"
     assetsDir: 'static', // Rename the assets folder from "assets" to "static"
@@ -17,4 +29,4 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-})
+});
